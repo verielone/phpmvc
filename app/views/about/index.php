482b5d497d0408d13879717119f7a3e0201487f5
verielone <savendavenda@gmail.com>
2021-11-28 20:24:14 +0700

    assets

--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,2 +1,8 @@
-<h1>About Index</h1>
-<p>Halo, nama saya <?= $data['nama'] ?>, umur saya <?= $data['umur'] ?> tahun, saya adalah seorang <?= $data['pekerjaan'] ?></p>
+<div class="container mt-5">
+    <div class="p-5 mb-4 mt-5 text-white bg-dark rounded-3">
+      <div class="container-fluid py-5">
+        <h1 class="display-5 fw-bold">About Index</h1>
+        <p class="col-md-12 fs-4">Halo, nama saya <?= $data['nama'] ?>, umur saya <?= $data['umur'] ?> tahun, saya adalah seorang <?= $data['pekerjaan'] ?></p>
+      </div>
+    </div>
+</div>
--- app/views/about/page.php
+++ app/views/about/page.php
@@ -1,1 +1,7 @@
-<h1>About Page</h1>
+<div class="container mt-5">
+    <div class="p-5 mb-4 mt-5 text-white bg-dark rounded-3">
+      <div class="container-fluid py-5">
+        <h1 class="display-5 fw-bold">About Page</h1>
+      </div>
+    </div>
+</div>
