88f239f9dca71a7d07b0e1c38357e920e092f2c3
verielone <savendavenda@gmail.com>
2021-11-28 17:53:30 +0700

    view

--- app/controllers/About.php
+++ app/controllers/About.php
@@ -1,11 +1,20 @@
 <?php
 
-class About {
-    public function index($nama='Manusia', $pekerjaan='Rebahan') {
-        echo "Halo, nama saya $nama, saya adalah seorang $pekerjaan";
+class About extends Controller{
+    public function index($nama='Manusia', $pekerjaan='Rebahan', $umur = 1) {
+        $data['nama'] = $nama;
+        $data['pekerjaan'] = $pekerjaan;
+        $data['umur'] = $umur;
+        $data['judul'] = 'About Index';
+        $this->view('templates/header', $data);
+        $this->view('about/index', $data);
+        $this->view('templates/footer');
     }
 
     public function page() {
-        echo 'About/page';
+        $data['judul'] = 'About Page';
+        $this->view('templates/header', $data);
+        $this->view('about/page');
+        $this->view('templates/footer');
     }
 }
