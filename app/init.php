5445284a08e9943ccd6bdaabfb598e34008b1ad0
verielone <savendavenda@gmail.com>
2021-12-07 20:34:48 +0700

    database wrapper

--- app/init.php
+++ app/init.php
@@ -2,7 +2,9 @@
 
 require_once 'core/App.php';
 require_once 'core/Controller.php';
-require_once 'core/Constants.php';
+require_once 'core/Database.php';
+
+require_once 'config/config.php';
 
 // spl_autoload_register(function( $class ){
 //     require_once __DIR__ . '/core/' . $class . '.php';
--- app/views/mahasiswa/index.php
+++ app/views/mahasiswa/index.php
@@ -2,14 +2,13 @@
     <div class="row">
         <div class="col-6">
             <h3>Daftar Mahasiswa</h3>
-            <?php foreach( $data['mhs'] as $mhs ) : ?>
-                <ul>
-                    <li><?= $mhs['nama']; ?></li>
-                    <li><?= $mhs['nim']; ?></li>
-                    <li><?= $mhs['email']; ?></li>
-                    <li><?= $mhs['jurusan']; ?></li>
+                <ul class="list-group">
+                    <?php foreach( $data['mhs'] as $mhs ) : ?>
+                        <li class="list-group-item d-flex justify-content-between align-items-center"><?= $mhs['nama']?>
+                            <a href="<?= BASEURL; ?>/mahasiswa/detail/<?= $mhs['id']?>" class="badge bg-dark">Detail</a>
+                        </li>
+                    <?php endforeach; ?>
                 </ul>
-            <?php endforeach; ?>
         </div>
     </div>
 </div>
